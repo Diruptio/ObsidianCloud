f0ee6d19e94df73cf5dcc60aa319a7a51b596798
Fabi.exe <66797305+FabiPunktExe@users.noreply.github.com>
2024-05-28 11:48:31 +0200

    Added SSL

--- common/src/main/java/de/obsidiancloud/common/network/SocketHandler.java
+++ common/src/main/java/de/obsidiancloud/common/network/SocketHandler.java
@@ -4,19 +4,33 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.channels.SocketChannel;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
 import org.jetbrains.annotations.NotNull;
 
+import javax.crypto.KeyGenerator;
+import javax.net.ssl.SSLSocket;
+
 public class SocketHandler {
-    private final SocketChannel socket;
+    private final SSLSocket socket;
     private final ObjectInputStream inputStream;
     private final ObjectOutputStream outputStream;
+    private byte[] privateKey;
+    private byte[] publicKey;
 
-    public SocketHandler(@NotNull SocketChannel socket) {
+    public SocketHandler(@NotNull SSLSocket socket) {
         this.socket = socket;
         try {
-            this.inputStream = new ObjectInputStream(socket.socket().getInputStream());
-            this.outputStream = new ObjectOutputStream(socket.socket().getOutputStream());
-        } catch (IOException exception) {
+            inputStream = new ObjectInputStream(socket.getInputStream());
+            outputStream = new ObjectOutputStream(socket.getOutputStream());
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            generator.initialize(2048);
+            KeyPair keyPair = generator.generateKeyPair();
+            privateKey = keyPair.getPrivate().getEncoded();
+            publicKey = keyPair.getPublic().getEncoded();
+        } catch (IOException | NoSuchAlgorithmException exception) {
             throw new RuntimeException(exception);
         }
     }
@@ -26,7 +40,7 @@ public class SocketHandler {
      *
      * @return The socket.
      */
-    public @NotNull SocketChannel getSocket() {
+    public @NotNull SSLSocket getSocket() {
         return socket;
     }
 
@@ -46,5 +60,23 @@ public class SocketHandler {
      */
     public @NotNull ObjectOutputStream getOutputStream() {
         return outputStream;
+    }
+
+    /**
+     * Get the public key.
+     *
+     * @return The public key.
+     */
+    public byte[] getPrivateKey() {
+        return privateKey;
+    }
+
+    /**
+     * Get the public key.
+     *
+     * @return The public key.
+     */
+    public byte[] getPublicKey() {
+        return publicKey;
     }
 }
