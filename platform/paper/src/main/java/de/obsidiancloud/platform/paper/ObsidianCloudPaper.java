cb32c4244244cb1613f284840271d057e8c6ca79
Fabi.exe <66797305+FabiPunktExe@users.noreply.github.com>
2024-06-17 18:54:56 +0200

    bugfix

--- platform/paper/src/main/java/de/obsidiancloud/platform/paper/ObsidianCloudPaper.java
+++ platform/paper/src/main/java/de/obsidiancloud/platform/paper/ObsidianCloudPaper.java
@@ -3,17 +3,19 @@ package de.obsidiancloud.platform.paper;
 import de.obsidiancloud.platform.ObsidianCloudPlatform;
 import de.obsidiancloud.platform.paper.listener.PlayerListener;
 import de.obsidiancloud.platform.paper.local.LocalPaperOCServer;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ObsidianCloudPaper extends JavaPlugin {
     @Override
     public void onEnable() {
-        ObsidianCloudPlatform.onEnable();
         String serverTask = System.getenv("OC_SERVER_TASK");
         String serverName = System.getenv("OC_SERVER_NAME");
         boolean serverAutoStart = Boolean.parseBoolean(System.getenv("OC_SERVER_AUTOSTART"));
         ObsidianCloudPlatform.onEnable(
                 new LocalPaperOCServer(serverTask, serverName, serverAutoStart));
+
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
     }
 
     @Override
