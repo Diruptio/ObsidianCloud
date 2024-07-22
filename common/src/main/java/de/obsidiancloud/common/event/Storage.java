3424d409bba6f109645c6eb07a148a1fac6877b6
Fabi.exe <MrStupsi@gmx.de>
2024-07-22 23:10:14 +0200

    created event storage

---
+++ common/src/main/java/de/obsidiancloud/common/event/Storage.java
@@ -0,0 +1,56 @@
+package de.obsidiancloud.common.event;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/** A storage for events. */
+public class Storage {
+    private final @NotNull Class<?> eventClass;
+    private final @NotNull Listener<Object> listener;
+    private @Nullable Storage next;
+
+    /**
+     * Create a new Storage.
+     *
+     * @param eventClass The class of the event.
+     * @param listener The listener that should be called.
+     * @param next The next storage.
+     */
+    @SuppressWarnings("unchecked")
+    public Storage(
+            @NotNull Class<?> eventClass, @NotNull Listener<?> listener, @Nullable Storage next) {
+        this.eventClass = eventClass;
+        this.listener = (Listener<Object>) listener;
+        this.next = next;
+    }
+
+    /**
+     * Call the listener if the event is the right class.
+     *
+     * @param event The event to call.
+     */
+    public void call(@NotNull Object event) {
+        if (eventClass == event.getClass()) {
+            listener.call(event);
+        }
+        if (next != null) next.call(event);
+    }
+
+    /**
+     * Get the next storage.
+     *
+     * @return The next storage.
+     */
+    public @Nullable Storage getNext() {
+        return next;
+    }
+
+    /**
+     * Set the next storage.
+     *
+     * @param next The next storage.
+     */
+    public void setNext(@Nullable Storage next) {
+        this.next = next;
+    }
+}
