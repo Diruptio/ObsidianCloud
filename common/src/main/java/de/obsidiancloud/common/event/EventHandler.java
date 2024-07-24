98f8f3a0a467578efedbf98e446904aed8344c13
Fabi.exe <MrStupsi@gmx.de>
2024-07-24 19:45:18 +0200

    new event system

--- common/src/main/java/de/obsidiancloud/common/event/EventHandler.java
+++ common/src/main/java/de/obsidiancloud/common/event/EventHandler.java
@@ -1,11 +1,10 @@
 package de.obsidiancloud.common.event;
 
-import org.jetbrains.annotations.Nullable;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.jetbrains.annotations.Nullable;
 
 /** An annotation to mark a method as an event handler. */
 @Target(ElementType.METHOD)
