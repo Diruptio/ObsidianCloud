212d277318efc0f4129940315e2cacb48b29c192
Fabi.exe <MrStupsi@gmx.de>
2024-09-01 09:58:49 +0200

    Added Node methods

--- node/src/main/java/de/obsidiancloud/node/remote/RemoteOCServer.java
+++ node/src/main/java/de/obsidiancloud/node/remote/RemoteOCServer.java
@@ -2,7 +2,10 @@ package de.obsidiancloud.node.remote;
 
 import de.obsidiancloud.common.OCServer;
 import de.obsidiancloud.common.network.packets.ServerStatusChangePacket;
+import de.obsidiancloud.common.network.packets.ServerUpdatePacket;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 
 public class RemoteOCServer extends OCServer {
@@ -29,6 +32,138 @@ public class RemoteOCServer extends OCServer {
     @Override
     public void kill() {
         // TODO: Send kill server packet to node
+    }
+
+    private void sendUpdatePacket(@NotNull TransferableServerData data) {
+        ServerUpdatePacket packet = new ServerUpdatePacket();
+        packet.setServerData(data);
+        node.getConnection().send(packet);
+    }
+
+    @Override
+    public void setAutoStart(boolean autoStart) {
+        sendUpdatePacket(
+                new TransferableServerData(
+                        data.task(),
+                        data.name(),
+                        data.type(),
+                        data.platform(),
+                        data.staticServer(),
+                        autoStart,
+                        data.executable(),
+                        data.memory(),
+                        data.args(),
+                        data.jvmArgs(),
+                        data.environmentVariables(),
+                        data.port()));
+    }
+
+    @Override
+    public void setExecutable(@NotNull String executable) {
+        sendUpdatePacket(
+                new TransferableServerData(
+                        data.task(),
+                        data.name(),
+                        data.type(),
+                        data.platform(),
+                        data.staticServer(),
+                        data.autoStart(),
+                        executable,
+                        data.memory(),
+                        data.args(),
+                        data.jvmArgs(),
+                        data.environmentVariables(),
+                        data.port()));
+    }
+
+    @Override
+    public void setMemory(int memory) {
+        sendUpdatePacket(
+                new TransferableServerData(
+                        data.task(),
+                        data.name(),
+                        data.type(),
+                        data.platform(),
+                        data.staticServer(),
+                        data.autoStart(),
+                        data.executable(),
+                        memory,
+                        data.args(),
+                        data.jvmArgs(),
+                        data.environmentVariables(),
+                        data.port()));
+    }
+
+    @Override
+    public void setJvmArgs(@NotNull List<String> jvmArgs) {
+        sendUpdatePacket(
+                new TransferableServerData(
+                        data.task(),
+                        data.name(),
+                        data.type(),
+                        data.platform(),
+                        data.staticServer(),
+                        data.autoStart(),
+                        data.executable(),
+                        data.memory(),
+                        data.args(),
+                        jvmArgs,
+                        data.environmentVariables(),
+                        data.port()));
+    }
+
+    @Override
+    public void setArgs(@NotNull List<String> args) {
+        sendUpdatePacket(
+                new TransferableServerData(
+                        data.task(),
+                        data.name(),
+                        data.type(),
+                        data.platform(),
+                        data.staticServer(),
+                        data.autoStart(),
+                        data.executable(),
+                        data.memory(),
+                        args,
+                        data.jvmArgs(),
+                        data.environmentVariables(),
+                        data.port()));
+    }
+
+    @Override
+    public void setEnvironmentVariables(@NotNull Map<String, String> environmentVariables) {
+        sendUpdatePacket(
+                new TransferableServerData(
+                        data.task(),
+                        data.name(),
+                        data.type(),
+                        data.platform(),
+                        data.staticServer(),
+                        data.autoStart(),
+                        data.executable(),
+                        data.memory(),
+                        data.args(),
+                        data.jvmArgs(),
+                        environmentVariables,
+                        data.port()));
+    }
+
+    @Override
+    public void setPort(int port) {
+        sendUpdatePacket(
+                new TransferableServerData(
+                        data.task(),
+                        data.name(),
+                        data.type(),
+                        data.platform(),
+                        data.staticServer(),
+                        data.autoStart(),
+                        data.executable(),
+                        data.memory(),
+                        data.args(),
+                        data.jvmArgs(),
+                        data.environmentVariables(),
+                        port));
     }
 
     @Override
